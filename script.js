617af8490c81c4dc141463cd43123495e5622af7
Thiago Omoto <thi.omoto@gmail.com>
2023-05-14 18:58:24 -0300

    fixing lint

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-let color = document.querySelectorAll('.color');
+const color = document.querySelectorAll('.color');
 
 function selectedColor(event) {
-    for (let index = 0; index < color.length; index += 1) {
-        color[index].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
 
 window.onload = function novaCor() {
-    for (let index = 0; index < color.length; index += 1) {
-        color[index].addEventListener('click', selectedColor);
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].addEventListener('click', selectedColor);
+  }
+
+  const pixelBoard = document.getElementById('pixel-board');
+};
+
+function corPixel() {
+  const boardPixel = document.getElementById('pixel-board');
+  boardPixel.addEventListener('click', (event) => {
+    const pixelSelect = event.target;
+    const colorSelected = document.querySelector('.selected');
+
+    if (pixelSelect.classList === 'pixel') {
+      pixelSelect.id = colorSelected.id;
     }
-
-    const pixelBoard = document.getElementById('pixel-board');
-}
-
-function corPixel () {
-    let boardPixel = document.getElementById('pixel-board');
-    boardPixel.addEventListener('click', function(event){
-        let pixelSelect = event.target;
-        let colorSelected = document.querySelector('.selected');
-        
-        if (pixelSelect.classList == 'pixel'){
-            pixelSelect.id = colorSelected.id;
-        }
-    })
+  });
 }
 corPixel();
 
-
-function limpandoPixel () {
-    let button = document.querySelector('#clear-board');
-    let pixel = document.querySelectorAll('.pixel');
-    button.addEventListener('click', function () {
-        for (let index = 0; index < pixel.length; index +=1){
-            pixel[index].id = document.getElementsByClassName('pixel');
-        }
-    })
+function limpandoPixel() {
+  const button = document.querySelector('#clear-board');
+  const pixel = document.querySelectorAll('.pixel');
+  button.addEventListener('click', () => {
+    for (let index = 0; index < pixel.length; index += 1) {
+      pixel[index].id = document.getElementsByClassName('pixel');
+    }
+  });
 }
 limpandoPixel();
